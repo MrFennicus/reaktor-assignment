63eebc6c06153fedd9d0688eb4181b419002fd69
Leevi Salin <salinleevi@gmail.com>
2021-12-20 00:13:11 +0200

    Fixed a bug in timestamp

--- frontend/src/ui/Time.js
+++ frontend/src/ui/Time.js
@@ -4,7 +4,7 @@ const Time = (props) => {
     const now = new Date()
     const time = new Date(props.time)
     let content
-    if(now.getDate() !== time.getDate()) content = `${time.getDay() + 1}.${time.getMonth()}.${time.getFullYear()}`
+    if(now.getDate() !== time.getDate()) content = `${time.getDate() + 1}.${time.getMonth() +1}.${time.getFullYear()}`
     else content = `${time.getHours()}:${time.getMinutes() < 10 ? '0' + time.getMinutes() : time.getMinutes()}`
     return (
             <span className={classes.Time}>{content}</span>
