1ae8b1d77ecf0e51530ea1654c8767be330d8f1e
Leevi Salin <salinleevi@gmail.com>
2021-12-23 17:57:28 +0200

    Fixed bug with time incorrectly showing time only: All dates with same day of moth as today were affected

--- frontend/src/ui/Time.js
+++ frontend/src/ui/Time.js
@@ -4,7 +4,8 @@ const Time = (props) => {
     const now = new Date()
     const time = new Date(props.time)
     let content = "" 
-    if(now.getDate() !== time.getDate()) content = `${time.getDate() }.${time.getMonth() +1}.${time.getFullYear()}`
+    const today = now.toDateString() === time.toDateString()
+    if(!today) content = `${time.getDate() }.${time.getMonth() +1}.${time.getFullYear()}`
     content += ` ${time.getHours()}:${time.getMinutes() < 10 ? '0' + time.getMinutes() : time.getMinutes()}`
     return (
             <span className={`${classes.Time} ${props.className}`}>{content}</span>
