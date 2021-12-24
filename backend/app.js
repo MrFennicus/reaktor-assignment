24fb27c1bed5e1c4485966dcea077aa098f7c2f4
Leevi Salin <salinleevi@gmail.com>
2021-12-24 12:34:14 +0200

    Refactored backend routing to a separate file

--- backend/app.js
+++ backend/app.js
@@ -1,60 +1,15 @@
-import { StandardWebSocketClient, WebSocketServer } from "./deps.js"
-import * as liveService from "./services/liveService.js"
-import * as playerService from "./services/playerService.js"
+import { StandardWebSocketClient } from "./deps.js"
+
+import { Router } from "./src/router.js"
 
 let port = 8080
 if (Deno.args.length > 0) {
     const lastArgument = Deno.args[Deno.args.length - 1]
     port = Number(lastArgument)
 }
+const router = new Router(port)
 console.log(`serving on ${port}`)
 
-const webSocketServer = new WebSocketServer(port)
-const webSocketClient = new StandardWebSocketClient(
-    "wss://bad-api-assignment.reaktor.com/rps/live"
-)
-playerService.fetchData(webSocketServer)
-webSocketClient.on("open", () => {
-    console.log("web socket client connected!")
-})
-webSocketClient.on("message", (message) => {
-    liveService.onMessage(message, webSocketServer)
-})
-webSocketClient.on("error", console.log)
 
-webSocketServer.on("connection", (ws) => {
-    const serveOnWebSocket = (data, requestId) => {
-        ws.send(
-            JSON.stringify(Object.assign({}, data, { requestId: requestId }))
-        )
-    }
 
-    // when connected, send out all currently ongoing games
-    serveOnWebSocket({ liveGames: liveService.getLiveGames() }, "live")
 
-    // serve data when requested by the client
-    ws.on("message", (message) => {
-        const data = JSON.parse(message)
-        const [request, ...args] = data.message.split("/")
-
-        // calls the given function with resolved arguments and serves the data
-        const route = (fnc, nofArgs) => {
-            if (args.length === nofArgs) {
-                serveOnWebSocket(fnc(...args), data.id)
-            } else ws.send(`invalid request ${message}`)
-        }
-
-        switch (request) {
-            case "games":
-                route(playerService.getGamesByPlayer, 2)
-                break
-            case "players":
-                route(playerService.getPlayerStats, 1)
-                break
-            default:
-                ws.send(`invalid request ${message}`)
-        }
-    })
-    ws.on("error", console.log)
-})
-webSocketServer.on("error", console.log)
---
+++ backend/src/router.js
@@ -0,0 +1,58 @@
+import * as playerService from "../services/playerService.js"
+import * as liveService from "../services/liveService.js"
+import { WebSocketServer, StandardWebSocketClient } from "../deps.js"
+
+export class Router {
+    constructor(port) {
+        this.webSocketServer = new WebSocketServer(port)
+        // send a message to clients when data is ready
+        playerService.fetchData().then(this.sendToAll)
+
+        this.webSocketServer.on("connection", (ws) => {
+            // when connected, send out all currently ongoing games
+            ws.send(this.respondWithData({ liveGames: liveService.getLiveGames() }, "live"))
+
+            // serve data when requested by the client
+            ws.on("message", (request) => ws.send(this.respondToRequest(request)))
+            ws.on("error", console.log)
+        })
+        this.webSocketServer.on("error", console.log)
+
+        this.webSocketClient = new StandardWebSocketClient(
+            "wss://bad-api-assignment.reaktor.com/rps/live"
+        )
+        this.webSocketClient.on("open", () => console.log("web socket client connected!"))
+        this.webSocketClient.on("message", (message) => {
+            const response = liveService.handleMessage(message)
+            if (response) this.sendToAll(response)
+        })
+        this.webSocketClient.on("error", console.log)
+    }
+    sendToAll = (data) => this.webSocketServer.clients.forEach((client) => client.send(data))
+
+    respondWithData = (data, requestId) => {
+        return JSON.stringify(Object.assign({}, data, { requestId: requestId }))
+    }
+
+    respondToRequest = (requestJSON) => {
+        const data = JSON.parse(requestJSON)
+        const [request, ...args] = data.message.split("/")
+
+        const respondWithFunction = (fnc, nofArgs) => {
+            if (args.length === nofArgs) {
+                return this.respondWithData(fnc(...args), data.id)
+            } else return `invalid request ${request}`
+        }
+
+        switch (request) {
+            case "games":
+                return respondWithFunction(playerService.getGamesByPlayer, 2)
+                break
+            case "players":
+                return respondWithFunction(playerService.getPlayerStats, 1)
+                break
+            default:
+                return `invalid request ${request}`
+        }
+    }
+}
